6abf31f52d68fe06066e5788968b313eb60d8499
Billy Maulana <billymaulana1999@gmail.com>
2022-10-23 02:58:32 +0700

    style: lintfix

--- server/api/movies/search/[query].ts
+++ server/api/movies/search/[query].ts
@@ -1,7 +1,6 @@
 const key = process.env.THE_MOVIE_DB_KEY
-
 export default defineEventHandler((event) => {
   const query = event.context.params.query
-  
+
   return fetch(`https://api.themoviedb.org/3/movie?query=${query}&page=1&api_key=${key}`).then(r => r.json())
 })
